7ef715ed667c9667f6238158b7c4527ae52f0373
mufiddin <m.mufiddin@gmail.com>
2024-07-13 11:17:28 +0700

    feat (author): add redis cache

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -8,7 +8,7 @@ use App\Http\Requests\AuthorQueryRequest;
 use App\Http\Requests\AuthorRequest;
 use App\Http\Resources\AuthorCollection;
 use App\Http\Resources\AuthorResource;
-
+use Illuminate\Support\Facades\Cache;
 
 class AuthorController extends Controller
 {
@@ -21,14 +21,22 @@ class AuthorController extends Controller
 
     public function index(AuthorQueryRequest $request)
     {
-        $authors = $this->authorRepository->all($request);
+        $queryString = $request->getQueryString();
+        $cacheKey = 'authors_all_' . md5($queryString);
+        $authors = Cache::remember($cacheKey, 90, function () use ($request) {
+            return $this->authorRepository->all($request);
+        });
         return new AuthorCollection($authors);
     }
 
     public function show($id)
     {
-        $author = $this->authorRepository->find($id);
-        $author || throw new ItemNotFoundException(config('message.errors.author_not_found'));
+        $cacheKey = "author_{$id}";
+        $author = Cache::remember($cacheKey, 90, function () use ($id) {
+            $author = $this->authorRepository->find($id);
+            $author || throw new ItemNotFoundException(config('message.errors.author_not_found'));
+            return $author;
+        });
 
         return new AuthorResource($author);
     }
@@ -36,6 +44,7 @@ class AuthorController extends Controller
     public function store(AuthorRequest $request)
     {
         $author = $this->authorRepository->create($request->all());
+        Cache::put("author_{$author->id}", $author, 90);
         return new AuthorResource($author);
     }
 
@@ -45,6 +54,7 @@ class AuthorController extends Controller
         $author || throw new ItemNotFoundException(config('message.errors.author_not_found'));
     
         $author = $this->authorRepository->update($id, $request->all());
+        Cache::put("author_{$id}", $author, 90); 
         return new AuthorResource($author);
     }
 
@@ -54,6 +64,7 @@ class AuthorController extends Controller
         $author || throw new ItemNotFoundException(config('message.errors.author_not_found'));
 
         $this->authorRepository->delete($author);
+        Cache::forget("author_{$id}");
         return response()->noContent();
     }
 }
